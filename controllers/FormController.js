304b8f562b12db728651b3eb358236e87f8f5e3f
Squel20 <77104410+Squel20@users.noreply.github.com>
2023-05-26 21:40:36 +0700

    controllers Form

---
+++ controllers/FormController.js
@@ -0,0 +1,135 @@
+import mongoose, { deleteModel } from "mongoose";
+import Form from "../models/Form.js";
+
+class FormController {
+  async index(req, res) {
+    try {
+      const form = await Form.find({
+        userId: req.jwt.id,
+      });
+
+      if (!form) {
+        throw { code: 404, message: "FORMS_NOT_FOUND" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "FORMS_FOUND",
+        total: form.length,
+        form,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const form = await Form.create({
+        userId: req.jwt.id,
+        title: "Untitled Form",
+        description: null,
+        public: true,
+      });
+      if (!form) {
+        throw { code: 400, message: "FAILD_CREATE_FORM" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "FORM_CREATE_SUCCESS",
+        form,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "ID_REQUIRED" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVALID_FORM_ID" };
+      }
+      const form = await Form.findOne({
+        _id: req.params.id,
+        userId: req.jwt.id,
+      });
+
+      if (!form) {
+        throw { code: 404, message: "FORM_NOT_FOUND" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "FORM_FOUND",
+        form,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "ID_REQUIRED" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVALID_FORM_ID" };
+      }
+      const form = await Form.findOneAndUpdate(
+        { _id: req.params.id, userId: req.jwt.id },
+        req.body,
+        { new: true }
+      );
+      if (!form) {
+        throw { code: 404, message: "FORM_UPDATE_FAILED" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "FORM_UPDATE_SUCCESS",
+        form,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
+  async destroy(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "ID_REQUIRED" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVALID_FORM_ID" };
+      }
+      const form = await Form.findOneAndDelete({
+        _id: req.params.id,
+        userId: req.jwt.id,
+      });
+      if (!form) {
+        throw { code: 404, message: "FORM_DELETE_FAILED" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "FORM_DELETE_SUCCESS",
+        form,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
+}
+export default new FormController();
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 import express from "express";
 import AuthController from "../controllers/AuthController.js";
 import jwtAuth from "../middlewares/jwtAuth.js";
+import FormController from "../controllers/FormController.js";
 const router = express.Router();
-
+//auth
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
-router.post("/refresh-token",jwtAuth(), AuthController.refreshToken);
+router.post("/refresh-token", jwtAuth(), AuthController.refreshToken);
+
+//form
+router.get("/forms", jwtAuth(), FormController.index);
+router.post("/forms", jwtAuth(), FormController.store);
+router.get("/forms/:id", jwtAuth(), FormController.show);
+router.put("/forms/:id", jwtAuth(), FormController.update);
+router.delete("/forms/:id", jwtAuth(), FormController.destroy);
+
 export default router;
