d87da0e15c8eb40be45a54eb1b2973eaf4acd1ca
Ilham <77104410+IlhamEl20@users.noreply.github.com>
2023-07-04 19:57:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.use((req, res) => {
 //database
 db();
 
-app.listen(env.APP_PORT, () => {
+app.listen(process.env.APP_PORT, () => {
   console.log(`server start ${process.env.APP_PORT}`);
 });
